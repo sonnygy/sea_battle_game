51cdfc2c63b95185c705c96ddf307d1ae8c9c340
Andrew <Adiksem2005@gmail.com>
2026-06-05 23:28:13 +0300

    BoardBuilder tweaks + ShipContainer for storing ships

--- src/main/java/org/miet/model/ShipPart.java
+++ src/main/java/org/miet/model/ShipPart.java
@@ -1,10 +1,7 @@
 package org.miet.model;
 
-// TODO:
-// --- Понять как связать состояние деталек и корабля (мб Observer)
-
-public class ShipPart { // Деталька размещается в клетке
-    private boolean hit; // Попали ли в детальку
+public class ShipPart {
+    private boolean hit;
 
     ShipPart(){
         hit = false;
